869aa86b0967d3b3002bc988a8469763fb5e7aba
yangjun <290968698@qq.com>
2022-10-24 10:13:33 +0800

    wait some time(default 10s) for vpn interface stable

--- cmd/socks5server/main.go
+++ cmd/socks5server/main.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"flag"
+	"time"
+
 	"github.com/things-go/go-socks5"
 )
 
 var flagInterface = flag.String("i", "", "interface to listen on")
 var flagPort = flag.String("p", "10800", "port to listen on")
+var flagWaitSeconds = flag.Int("w", 10, "wait seconds before listen on port")
+
+var Version = "0.0.2"
 
 func main() {
+	println("socks5server version:", Version)
 	flag.Parse()
+	time.Sleep(time.Duration(*flagWaitSeconds) * time.Second)
 	// Create a SOCKS5 server
 	server := socks5.NewServer()
 
